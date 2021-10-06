4afa2903c5c0bec0450741c38e97f170e5a503f7
msamgan <mohdsamgankhan@gmail.com>
2021-10-06 10:31:11 +0400

    updated git ignore

--- middlewares/validators/index.js
+++
@@ -1,9 +0,0 @@
-const indexValidator = require("./index.validations");
-
-/**
- *
- * @type {{indexValidator?: (ValidationChain|(function(*=, *, *): (*|undefined)))[]}}
- */
-module.exports = {
-    ...indexValidator
-}
