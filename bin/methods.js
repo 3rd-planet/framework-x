ae07ae47c3b0aa63959200f9ac76c30e5d2a1919
Mohd Samgan Khan <mohdsamgankhan@gmail.com>
2021-10-08 20:08:03 +0400

    upgraded the installer for promt questions (#22)

---
+++ bin/methods.js
@@ -0,0 +1,42 @@
+const fs = require("fs");
+const util = require("util");
+const exec = util.promisify(require('child_process').exec)
+
+/**
+ *
+ * @param command
+ * @returns {Promise<void>}
+ */
+exports.runCmd = async command => {
+    try {
+        const {stdout, stderr} = await exec(command)
+        console.log('\x1b[33m', 'Executing : ' + command, '\x1b[0m')
+    } catch (error) {
+        console.log('\x1b[31m', error.message, '\x1b[0m')
+    }
+}
+
+/**
+ *
+ * @param appPath
+ * @param folderName
+ * @returns {Promise<void>}
+ */
+exports.checkDirExist = async (appPath, folderName) => {
+    try {
+        fs.mkdirSync(appPath)
+    } catch (err) {
+
+        if (err.code.toString() === 'EEXIST') {
+            console.log(
+                '\x1b[31m',
+                `The file ${folderName} already exist in the current directory, please give it another name.`,
+                '\x1b[0m'
+            )
+        } else {
+            console.log(err)
+        }
+
+        process.exit(1)
+    }
+}
