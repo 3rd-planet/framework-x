f68df9e8326ecb3c02eeb9f71ad36e1464b383f7
msamgan <mohdsamgankhan@gmail.com>
2021-10-09 23:41:26 +0400

    text update

--- tests/index.fail.validation.test.js
+++ tests/index.fail.validation.test.js
@@ -1,11 +1,11 @@
 const app = require("../index");
 const supertest = require("supertest");
 
-describe("Test the root post path", () => {
-    test("It should response the POST method", async () => {
+describe("Test the root post path with fail validation", () => {
+    test("It should response the POST method with failed validation", async () => {
         await supertest(app).post("/")
             .send({
-                "ky": "value"
+                "ky": "value" //key is requited in validation...
             })
             .expect(406)
             .then((response) => {
