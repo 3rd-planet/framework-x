b6943e590a0765f5418cebb375434c61df4d38c7
Mohd Samgan Khan <mohdsamgankhan@gmail.com>
2021-10-17 16:32:52 +0400

    Jest Test Suite

--- index.js
+++ index.js
@@ -3,16 +3,12 @@ const errorMiddleware = require('./middlewares/error.middleware');
 
 const express = require('express')
 const app = express()
-const port = process.env.PORT
 
 app.use(express.json());
-
-console.log(`Node environment: ${process.env.NODE_ENV}`);
-app.listen(port, () => {
-    console.log(`Example app listening at port http://localhost:${port}`)
-})
 
 require('./routes/api')(app);
 
 // Error Handler Middleware
 app.use(errorMiddleware);
+
+module.exports = app
