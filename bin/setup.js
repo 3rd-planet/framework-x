2ee49832674b5681e0c68e9f30b333ba0cc6f085
HudaQeshta <huda.qeshta@shopiniworld.com>
2021-10-17 13:29:09 +0400

    added mongo db support

--- bin/setup.js
+++ bin/setup.js
@@ -1,52 +1,61 @@
-const {runCmd} = require("./methods");
+const { runCmd } = require("./methods");
 const fs = require("fs");
 const path = require("path");
 
 exports.setup = async (answers) => {
-    try {
+	try {
 
-        console.log('\x1b[33m', 'Downloading the project structure...', '\x1b[0m')
-        await runCmd(`git clone --depth 1 ${answers.repo} ${answers.app_name}`)
+		console.log('\x1b[33m', 'Downloading the project structure...', '\x1b[0m')
+		await runCmd(`git clone --depth 1 ${answers.repo} ${answers.app_name}`)
 
-        process.chdir(answers.app_path)
+		process.chdir(answers.app_path)
 
-        console.log(
-            '\x1b[32m',
-            'Creating Environment... !',
-            '\x1b[0m'
-        );
-        await fs.copyFileSync('./.env.example', '.env')
+		console.log(
+			'\x1b[32m',
+			'Creating Environment... !',
+			'\x1b[0m'
+		);
+		await fs.copyFileSync('./.env.example', '.env')
 
-        console.log('\x1b[34m', 'Installing dependencies...', '\x1b[0m')
+		console.log('\x1b[34m', 'Installing dependencies...', '\x1b[0m')
 
-        await runCmd('npm install --silent')
+		await runCmd('npm install --silent')
 
-        if (answers.sequelize) {
-            await runCmd('npm install --save sequelize')
-            await runCmd('npx sequelize-cli init --force')
+		if (answers.db_support) {
+			if ((typeof answers.db_support_options !== 'undefined')) {
+				if (['mysql', 'sqlite'].includes(answers.db_support_options)) {
+					await runCmd('npm install --save sequelize')
+					await runCmd('npx sequelize-cli init --force')
 
-            if ((typeof answers.sequelize_options !== 'undefined') && answers.sequelize_options === 'mysql') {
-                await runCmd('npm install --save mysql2')
-            }
+					if (answers.db_support_options === 'mysql') {
+						await runCmd('npm install --save mysql2')
+					}
 
-            if ((typeof answers.sequelize_options !== 'undefined') && answers.sequelize_options === 'sqlite') {
-                await runCmd('npm install --save sqlite3')
-                await fs.copyFileSync('./bin/files/sqlite.config.json', './config/config.json')
-            }
-        }
+					if (answers.db_support_options === 'sqlite') {
+						await runCmd('npm install --save sqlite3')
+						await fs.copyFileSync('./bin/files/sqlite.config.json', './config/config.json')
+					}
+				} else {
+					if (answers.db_support_options === 'mongodb') {
+						await runCmd('npm install --save mongoose')
+						await fs.copyFileSync('./bin/files/mongodb-connection.js', './config/db-connection.js')
+					}
+				}
+			}
+		}
 
-        console.log('\x1b[34m', 'Removing unwanted files...', '\x1b[0m')
+		console.log('\x1b[34m', 'Removing unwanted files...', '\x1b[0m')
 
-        await fs.rmdirSync(path.join(answers.app_path, '.git'), {recursive: true})
-        await fs.rmdirSync(path.join(answers.app_path, 'bin'), {recursive: true})
-        await fs.rmdirSync(path.join(answers.app_path, 'docs'), {recursive: true})
-        await fs.unlinkSync('LICENSE');
-        await fs.unlinkSync('README.md');
-        await fs.unlinkSync('./mkdocs.yml');
+		await fs.rmdirSync(path.join(answers.app_path, '.git'), { recursive: true })
+		await fs.rmdirSync(path.join(answers.app_path, 'bin'), { recursive: true })
+		await fs.rmdirSync(path.join(answers.app_path, 'docs'), { recursive: true })
+		await fs.unlinkSync('LICENSE');
+		await fs.unlinkSync('README.md');
+		await fs.unlinkSync('./mkdocs.yml');
 
-    } catch (error) {
-        console.log(error)
+	} catch (error) {
+		console.log(error)
 
-        process.exit(1)
-    }
+		process.exit(1)
+	}
 }
