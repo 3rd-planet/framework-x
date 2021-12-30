00cb86cf010b188a85f07c9067dde0f60e57af1b
msamgan <mohdsamgankhan@gmail.com>
2021-12-31 02:21:20 +0400

    added

--- index.js
+++ index.js
@@ -14,11 +14,6 @@ app.use(express.json());
 // Req and Res logger.
 app.use(accessLogMiddleware);
 
-console.log(`Node environment: ${process.env.NODE_ENV}`);
-app.listen(port, () => {
-    console.log(`Example app listening at port http://localhost:${port}`)
-})
-
 require('./routes/api')(app);
 
 // Error Handler Middleware
