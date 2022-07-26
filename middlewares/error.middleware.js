1a00ca6f1210a4950ec6636faefde9adb3538d4e
Mohd Samgan Khan <mohdsamgankhan@gmail.com>
2022-07-27 01:22:50 +0400

    formatting and MongoDB Bug (#56)

--- middlewares/error.middleware.js
+++ middlewares/error.middleware.js
@@ -1,9 +1,6 @@
 const methods = require("../helpers/methods")
 const errorLog = require("simple-node-logger").createSimpleLogger({
-    logFilePath:
-        "./log/error/" +
-        new Date().toLocaleDateString().split("/").join("-") +
-        ".log",
+    logFilePath: "./log/error/" + new Date().toLocaleDateString().split("/").join("-") + ".log",
     timestampFormat: "YYYY-MM-DD HH:mm:ss"
 })
 /**
@@ -17,7 +14,5 @@ const errorLog = require("simple-node-logger").createSimpleLogger({
 // eslint-disable-next-line no-unused-vars
 module.exports = (error, req, res, next) => {
     errorLog.error(error.message)
-    return res
-        .status(process.env.EXCEPTION_CODE)
-        .send(methods.failResponse(error.message))
+    return res.status(process.env.EXCEPTION_CODE).send(methods.failResponse(error.message))
 }
--- tests/index.fail.validation.test.js
+++ tests/index.fail.validation.test.js
@@ -1,15 +1,16 @@
-const app = require("../index");
-const supertest = require("supertest");
+const app = require("../index")
+const supertest = require("supertest")
 
 describe("Test the root post path with fail validation", () => {
     test("It should response the POST method with failed validation", async () => {
-        await supertest(app).post("/")
+        await supertest(app)
+            .post("/")
             .send({
-                "ky": "value" //key is requited in validation...
+                ky: "value" //key is requited in validation...
             })
             .expect(406)
             .then((response) => {
                 expect(response.body.status).toBe(false)
-            });
-    });
-});
+            })
+    })
+})
