801f7f0fa1d585dcd04427fd4b406f8c9ead4dd0
msamgan <mohdsamgankhan@gmail.com>
2023-09-05 14:50:38 -0400

    basic server.ts config.

--- index.ts
+++ index.ts
@@ -1,16 +1,14 @@
-import express, { Express, Request, Response, Application } from "express"
 import dotenv from "dotenv"
+import express, { Express, Request, Response, Application } from "express"
 
 //For env File
 dotenv.config()
 
 const app: Application = express()
-const port = process.env.PORT || 8000
 
 app.get("/", (req: Request, res: Response) => {
     res.send("Welcome to Express & TypeScript Server")
 })
 
-app.listen(port, () => {
-    console.log(`Server is Fire at http://localhost:${port}`)
-})
+
+export default app
---
+++ server.ts
@@ -0,0 +1,9 @@
+import app from "./index"
+
+const port = process.env.PORT || 8000
+
+console.log(`Node environment: ${process.env.NODE_ENV}`)
+app.listen(port, () => {
+        console.log(`Example app listening at port http://localhost:${port}`)
+    }
+)
