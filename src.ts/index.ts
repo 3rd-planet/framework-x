50a05d6819a2e9cccb23595226f60b86af1390c4
msamgan <mohdsamgankhan@gmail.com>
2023-09-05 16:56:08 -0400

    added access log

--- src.ts/index.ts
+++ src.ts/index.ts
@@ -1,7 +1,10 @@
 import dotenv from "dotenv"
+import "express-async-errors"
 import express, { Application } from "express"
 import cors from "cors"
 import { loadRoutes } from "./helpers/methods"
+
+import accessLogMiddleware from "./middlewares/logger.middleware"
 
 dotenv.config()
 
@@ -10,6 +13,8 @@ const app: Application = express()
 app.use(cors())
 
 app.use(express.json())
+
+app.use(accessLogMiddleware)
 
 loadRoutes(app)
 
