b9116c58822dff5ee5f4233627d1cfa90813bcf1
msamgan <mohdsamgankhan@gmail.com>
2023-09-06 15:32:51 -0400

    updated installer

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,4 @@
-FROM node:14.16
+FROM node:18.16
 WORKDIR /app
 COPY package*.json ./
 RUN npm i
--- bin/setup.js
+++ bin/setup.js
@@ -24,17 +24,25 @@ const dependencies = [
  */
 const devDependencies = ["jest", "nodemon", "supertest", "husky"]
 
+const tsDevDependencies = ["@types/jest", "@types/supertest", "@types/cors", "@types/express", "ts-jest", "typescript", "ts-node", "ts-node-dev"]
+
 /**
  *
  * @returns {Promise<void>}
  */
-const dependenciesInstall = async () => {
+const dependenciesInstall = async (app_mode, app_package_manager) => {
     for (const dependency of dependencies) {
-        await runCmd("npm install --save " + dependency)
+        await runCmd(app_package_manager + " install --save " + dependency)
     }
 
     for (const dependency of devDependencies) {
-        await runCmd("npm install --save-dev " + dependency)
+        await runCmd(app_package_manager + " install --save-dev " + dependency)
+    }
+
+    if (app_mode === "typescript") {
+        for (const dependency of tsDevDependencies) {
+            await runCmd(app_package_manager + " install --save-dev " + dependency)
+        }
     }
 }
 
@@ -66,38 +74,101 @@ const dbInstall = async (db_support_options) => {
     }
 }
 
-exports.setup = async ({ app_path, db_support, db_support_options, clone_command }) => {
+const updatePackageJson = async (
+    app_mode,
+    app_orm,
+    app_db,
+    app_package_manager
+) => {
+    let fileExtension = app_mode === "typescript" ? "ts" : "js"
+    let packageJson = JSON.parse(fs.readFileSync("./package.json", "utf8"))
+    packageJson.xconfig = {
+        mode: app_mode,
+        orm: app_orm,
+        db: app_db,
+        package_manager: app_package_manager
+    }
+
+    packageJson.main = "app/server." + fileExtension
+
+    if (fileExtension === "ts") {
+        packageJson.jest = {
+            "preset": "ts-jest",
+            "testEnvironment": "node"
+        }
+
+        packageJson.scripts = {
+            ...packageJson.scripts,
+            "build": "npx tsc"
+        }
+    }
+
+    fs.writeFileSync("./package.json", JSON.stringify(packageJson, null, 4))
+}
+
+exports.setup = async ({ app_path, app_mode, app_orm, app_db, app_package_manager, clone_command, docker_support }) => {
     try {
         await runCmd(clone_command)
         process.chdir(app_path)
         await fs.copyFileSync("./.env.example", ".env")
         await fs.copyFileSync("./bin/files/package.json", "./package.json")
 
-        await dependenciesInstall()
+        await updatePackageJson(app_mode, app_orm, app_db, app_package_manager)
+        await dependenciesInstall(app_mode, app_package_manager)
 
-        if (db_support && typeof db_support_options !== "undefined") {
+        let sourceDir = app_mode === "typescript" ? "./src.ts" : "./src.js"
+        let destinationDir = "./app"
+
+        await fs.cpSync(sourceDir, destinationDir, {
+            recursive: true
+        })
+
+        /*if (db_support && typeof db_support_options !== "undefined") {
             await dbInstall(db_support_options)
+        }*/
+
+        let direToRemove = [
+            ".git",
+            ".github",
+            "bin",
+            "src.js",
+            "src.ts"
+        ]
+
+        let filesToRemove = [
+            "README.md",
+            "CODE_OF_CONDUCT.md",
+            "CONTRIBUTING.md",
+            "SECURITY.md",
+            "LICENSE",
+            "app/package.json"
+        ]
+
+        if (app_mode !== "typescript") {
+            filesToRemove.push("tsconfig.json")
         }
 
-        await fs.rmSync(path.join(app_path, ".git"), {
-            recursive: true
-        })
-        await fs.rmSync(path.join(app_path, "bin"), {
-            recursive: true
-        })
+        if (!docker_support) {
+            filesToRemove.push(".dockerignore")
+            filesToRemove.push("Dockerfile")
+        }
+
+        for (const dir of direToRemove) {
+            await fs.rmSync(path.join(app_path, dir), {
+                recursive: true
+            })
+        }
+
+        for (const file of filesToRemove) {
+            await fs.unlinkSync(path.join(app_path, file))
+        }
 
         /**
          * introducing Husky to the system...
          */
         await runCmd("git init")
         await runCmd("npx husky-init")
-        await runCmd("pnpm install")
-
-        await fs.unlinkSync("README.md")
-        await fs.unlinkSync("./CODE_OF_CONDUCT.md")
-        await fs.unlinkSync("./CONTRIBUTING.md")
-        await fs.unlinkSync("./SECURITY.md")
-        await fs.unlinkSync("LICENSE")
+        await runCmd(app_package_manager + " install")
     } catch (error) {
         console.log(error)
 
